42e7a7ec851f1947a9931039f2ae42bed2fe05bb
Tiffany Liu <59628681+ttliu00@users.noreply.github.com>
2024-05-02 10:44:53 -0400

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,3 +1,4 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("dplyr","data.table","tidyr"), repos="https://cran.r-project.org", dependencies=TRUE)
+install.packages(c("dplyr","data.table","tidyr","caret","boot","glmnet","MLmetrics"), repos="https://cran.r-project.org", dependencies=TRUE)
+install.packages(c("dplyr","data.table","tidyr"), repos="https://cran.r-project.org")
